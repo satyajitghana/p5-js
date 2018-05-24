1b9d3e701bdceaa5d5cf8ff42854c397a52c814a
Satyajit Ghana <satyajitghana7@gmail.com>
2018-05-25 00:01:33 +0530

    added sliders :stuck_out_tongue:

--- Phyllotaxis/Phyllotaxis.js
+++ Phyllotaxis/Phyllotaxis.js
@@ -5,23 +5,39 @@ var button;
 var buttonAngle;
 var input;
 var defAng;
-var off = random(255);
+var off;
+var sizeSlider;
+var cSlider;
 
 function setup() {
   createCanvas(800, 800);
   angleMode(DEGREES);
   colorMode(HSB, 255);
   clearSketch();
+  
   button = createButton("Reset Sketch");
   button.position(0, 800);
   button.mousePressed(clearSketch);
+  
   input = createInput("137.5");
   input.position(button.x+button.width+50, 800);
+  
   buttonAngle = createButton("Set Angle");
   buttonAngle.position(input.x+input.width, 800);
   buttonAngle.mousePressed(setAngle);
+  sizeSlider = createSlider(1, 20, 8, 0.1);
+  
+  sizeSlider.position(0, 850);
+  var sizeText = createElement("h4", "Size");
+  sizeText.position(sizeSlider.x*2+sizeSlider.width + 30, 825);
+  
+  cSlider = createSlider(1, 20, 8, 0.1);
+  cSlider.position(0, 880);
+  var cText = createElement("h4", "Inter-Distance");
+  cText.position(sizeSlider.x*2+sizeSlider.width + 30, 865);
+  
   defAngle = createElement("h4", "Default Angle = 137.5");
-  defAngle.position(width/2-80, 850); 
+  defAngle.position(width/2-20, 850); 
   background(0);
 }
 
@@ -39,16 +55,18 @@ function clearSketch() {
 }
 
 function draw() {
-  translate(width/2, height/2);
+  //translate(width/2, height/2);
   //var phi = n * 137.5 * PI / 180;
+  c = cSlider.value();
   var phi = n * angle;
   var r = c * sqrt(n);
   n++;
-  
-  var x = r * cos(phi);
-  var y = r * sin(phi);
+  var x = (width/2) + r * cos(phi);
+  var y = (height/2) + r * sin(phi);
+  //var c = map((off+r), 0, 400, 0, 255);
   noStroke();
   fill((off+r)%256, 255, 255);
-  ellipse(x, y, 8, 8);
+  //fill(c, 255, 255);
+  ellipse(x, y, sizeSlider.value(), sizeSlider.value());
   
 }
